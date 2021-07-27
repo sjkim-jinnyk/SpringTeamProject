9b44ea1312727f1e511de9074662cc09773fff86
csooy <csooy38@gmail.com>
2021-07-27 11:10:42 +0900

    UTBT 0727 관리자 사이드바 토글 수정(main.js)

--- SpringProject/src/main/webapp/resources/js/main.js
+++ SpringProject/src/main/webapp/resources/js/main.js
@@ -117,3 +117,23 @@ function slide() {
   init();
   slideEvent();
 }
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
